47437424d9252572064dbe98f7cde7677c41ab4a
Graham McIntire <graham@mcintire.me>
2025-01-27 13:59:42 -0600

    try docker build

--- config/config.exs
+++ config/config.exs
@@ -40,7 +40,7 @@ config :aprs,
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.17.11",
+  version: "0.24.2",
   default: [
     args: ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
