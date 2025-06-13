587feef1c1393fc6e596b1f9c89ff05413a12127
Graham McIntire <graham@mcintire.me>
2025-06-13 16:28:20 -0500

    add spot clustering

--- lib/aprs_web/live/map_live/index.ex
+++ lib/aprs_web/live/map_live/index.ex
@@ -118,11 +118,21 @@ defmodule AprsWeb.MapLive.Index do
       integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
       crossorigin=""
     />
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.Default.css"
+    />
     <script
       src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
       integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
       crossorigin=""
     >
+    </script>
+    <script src="https://unpkg.com/leaflet.markercluster@1.5.3/dist/leaflet.markercluster.js">
     </script>
 
     <style>
