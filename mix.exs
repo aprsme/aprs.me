909829d053ae2b132705644f7e9a638b20856a34
Graham McIntire <graham@mcintire.me>
2025-06-13 10:42:36 -0500

    drop to elixir 1.17

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Aprs.MixProject do
     [
       app: :aprs,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
