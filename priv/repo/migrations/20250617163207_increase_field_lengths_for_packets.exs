4c0433d32ed05cdd5fb46a16a499a28fa1a0ac0d
Graham McIntire <graham@mcintire.me>
2025-06-17 11:32:52 -0500

    increase packet length

---
+++ priv/repo/migrations/20250617163207_increase_field_lengths_for_packets.exs
@@ -0,0 +1,14 @@
+defmodule Aprs.Repo.Migrations.IncreaseFieldLengthsForPackets do
+  use Ecto.Migration
+
+  def change do
+    alter table(:packets) do
+      # Change fields that might contain long strings from varchar(255) to text
+      modify :information_field, :text, from: :string
+      modify :path, :text, from: :string
+      modify :manufacturer, :text, from: :string
+      modify :equipment_type, :text, from: :string
+      modify :addressee, :text, from: :string
+    end
+  end
+end
