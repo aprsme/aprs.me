969dc1ee81ceb0ead6c7c3e31f1daafeb69eb7d9
Fly Dev <dev@fly.io>
2025-06-23 14:21:16 +0000

    update MockHelpers to stub all expected functions

--- test/support/mock_helpers.ex
+++ test/support/mock_helpers.ex
@@ -11,6 +11,10 @@ defmodule Aprs.MockHelpers do
 
     Mox.stub(PacketsMock, :get_historical_packet_count, fn _opts -> 0 end)
     Mox.stub(PacketsMock, :stream_packets_for_replay, fn _opts -> [] end)
+    Mox.stub(PacketsMock, :get_packets_for_replay, fn _opts -> [] end)
+    Mox.stub(PacketsMock, :get_recent_packets, fn _opts -> [] end)
+    Mox.stub(PacketsMock, :clean_old_packets, fn -> {:ok, 0} end)
+    Mox.stub(PacketsMock, :clean_packets_older_than, fn _days -> {:ok, 0} end)
   end
 
   def stub_badpackets_mock do
