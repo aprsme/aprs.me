a3d593a4c1f72c77232fc41e86bc8e09b4bbbac8
Graham McIntire <graham@mcintire.me>
2025-07-05 09:25:34 -0500

    deploy fix for tailwind

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,8 +1,6 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-const plugin = require("tailwindcss/plugin")
-
 module.exports = {
   content: [
     "./js/**/*.js",
@@ -18,10 +16,6 @@ module.exports = {
   },
   plugins: [
     require("@tailwindcss/forms"),
-    require("daisyui"),
-    plugin(({addVariant}) => addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])),
-    plugin(({addVariant}) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
-    plugin(({addVariant}) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"]))
+    require("daisyui")
   ]
 }
