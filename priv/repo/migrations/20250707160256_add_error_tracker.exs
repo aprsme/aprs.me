ad18f641b8c84afea86eb441ec9c0035f3a6f0bf
Graham McIntire <graham@mcintire.me>
2025-07-07 11:06:35 -0500

    include migration

---
+++ priv/repo/migrations/20250707160256_add_error_tracker.exs
@@ -0,0 +1,8 @@
+defmodule Aprsme.Repo.Migrations.AddErrorTracker do
+  use Ecto.Migration
+
+  def up, do: ErrorTracker.Migration.up(version: 5)
+
+  # We specify `version: 1` in `down`, to ensure we remove all migrations.
+  def down, do: ErrorTracker.Migration.down(version: 1)
+end
