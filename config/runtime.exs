1c7a1754ee386534d119ea41f0a54c475bc2fa61
Graham McIntire <graham@mcintire.me>
2025-07-18 15:23:58 -0500

    add sentry-cdn to cors

--- config/runtime.exs
+++ config/runtime.exs
@@ -104,7 +104,7 @@ if config_env() == :prod do
     ],
     secret_key_base: secret_key_base,
     server: true,
-    check_origin: ["https://#{host}", "http://10.0.19.222:33897", "https://s.aprs.me"]
+    check_origin: ["https://#{host}", "http://10.0.19.222:33897", "https://s.aprs.me", "https://js.sentry-cdn.com"]
 
   # Optional: Set the default "from" email address
   config :aprsme,
